f78a4d8667b130b004255225db044da479b7164f
Pedro Victor Abreu <pedro.fabreu97@gmail.com>
2025-05-21 00:23:07 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,3 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/login', function () {
-    return response()->json([
-        'message' => 'Por favor, faça login para acessar este recurso.',
-    ], 401);
-})->name('login');
